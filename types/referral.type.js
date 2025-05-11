ea9eb148cb0037d1d9224b2661f529ff6ee9b59e
Pratham2403 <prathamaggarwal2403@gmail.com>
2025-05-11 05:42:24 +0530

    Add referral status and mode types

---
+++ types/referral.type.js
@@ -0,0 +1,15 @@
+//        enum:['pending','active','removed','expired'],
+
+
+export const ReferralStatus = {
+    PENDING:"Pending",
+    ACTIVE:"Active",
+    REJECTED:"Rejected",
+    EXPIRED:"Expired",
+}
+
+export const ReferralMode = {
+    REMOTE:"Remote",
+    HYBRID:"Hybrid",
+    ONSITE:"Onsite",
+}
